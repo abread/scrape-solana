45951572b3f8c951e56f1487553177daae00cafa
André Breda <andrebreda@tecnico.ulisboa.pt>
2025-02-07 17:31:46 +0000

    flake: update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs = {
