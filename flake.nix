6db86be324cd1bf58db82b907fe54f2b9b04a854
André Breda <andrebreda@tecnico.ulisboa.pt>
2025-03-26 14:42:55 +0000

    flake: build project and container image

--- flake.nix
+++ flake.nix
@@ -8,15 +8,30 @@
         nixpkgs-stable.follows = "nixpkgs";
       };
     };
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
+    crane,
+    fenix,
     ...
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    craneLib = (crane.mkLib pkgs).overrideToolchain (fenix.packages.${system}.stable.withComponents ["cargo" "rustc"]);
+
+    scraperSysDeps = with pkgs; [
+      pkg-config
+      zstd
+      openssl
+    ];
   in {
     checks.${system}.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
       src = ./.;
@@ -56,12 +71,38 @@
       inherit (self.checks.${system}.pre-commit-check) shellHook;
       buildInputs =
         self.checks.${system}.pre-commit-check.enabledPackages
+        ++ scraperSysDeps
         ++ (with pkgs; [
-          pkg-config
-          zstd
           rustup
-          openssl
         ]);
+    };
+
+    packages.${system} = {
+      scrape-solana = craneLib.buildPackage {
+        pname = "scrape-solana";
+        version = "latest";
+        src = craneLib.cleanCargoSource ./.;
+        buildInputs = scraperSysDeps;
+
+        # it's a workspace, we need to select the right thing
+        cargoExtraArgs = "-p scrape-solana";
+
+        # panic on
+        cargoBuildCommand = "cargo build --profile release --config debug=\\\"full\\\" --config overflow-checks=true --config lto=\\\"thin\\\"";
+      };
+      img-builder = pkgs.dockerTools.streamLayeredImage {
+        name = "scrape-solana";
+        tag = "latest";
+
+        extraCommands = ''
+          mkdir -p data
+        '';
+
+        config = {
+          Entrypoint = ["${self.packages.${system}.scrape-solana}/bin/scrape-solana"];
+          WorkingDir = "/data";
+        };
+      };
     };
 
     formatter.${system} = pkgs.alejandra;
